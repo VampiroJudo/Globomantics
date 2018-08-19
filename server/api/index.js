a43166b86285d302dd67f4878e1bc749747cec40
VampiroJudo <johnconley507@hotmail.com>
2018-08-19 12:26:27 -0400

    Major bug fixes

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+var cors = require('cors');
 const router = express.Router();
+
 
 require('./routes/transaction')(router);
 require('./routes/user')(router);
 
-module.exports = router; 
+module.exports = router;
